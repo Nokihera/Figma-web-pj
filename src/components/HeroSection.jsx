16ca257b33b964cc51025840d8e57280d7854c77
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-17 16:28:46 +0630

    heroSection and cardOne.jsx is changed resposive .And About page is working on but not footer.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,72 +2,65 @@ import React from "react";
 import CardOne from "./cards/CardOne";
 
 const HeroSection = ({ conents }) => {
-  const cardData={
-    title:"How to start a blog in 2024: The ultimate guide ",
-    text : "Discover expect tips, latest trends, and valuable insights in digital design and project management",
-    isBtn:true,
-  }
   return (
-    <div className="w-full h-[938.66px] bg-no-repeat bg-center bg-cover flex flex-col items-center content-start bg-gradient-to-b from-herobgColor">
-      <div className="w-[885px] h-[475px] py-[80px] flex flex-col justify-center items-center">
-        <p className="text-[#6596A1] font-futura text-[22px]">
-          {conents.pagename}
+    <div className="w-full h-auto min-h-screen bg-no-repeat bg-center bg-cover flex flex-col items-center content-start bg-gradient-to-b from-herobgColor">
+    <div className="w-full max-w-[885px] h-auto py-10 flex flex-col justify-center items-center">
+      <p className="text-[#6596A1] font-futura text-lg sm:text-xl">
+        {conents.pagename}
+      </p>
+      <div className="flex flex-col items-center">
+        <p className="text-4xl sm:text-5xl md:text-6xl font-futura text-center text-[#223236] font-bold">
+          {conents.title}
         </p>
-        <div className="flex flex-col items-center ">
-          <p className="text-[61px] font-futura text-center text-[#223236] font-bold">
-            {conents.title}
+        <div className="w-11/12 max-w-[521px]">
+          <p className="text-center text-[#7F7D7D] text-base sm:text-lg md:text-xl mb-5">
+            {conents.text}
           </p>
-          <div className="w-[521px]">
-            <p className="text-center text-[#7F7D7D] text-[20px] mb-[20px]">
-              {conents.text}
-            </p>
+        </div>
+        {conents.isButton && (
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-8">
+            <button className="border border-[#6596A1] rounded-md px-5 py-2 text-[#6596A1] font-lato">
+              Learn More
+            </button>
+            <button className="bg-[#6596A1] rounded-md py-2 px-5 text-white w-full sm:w-auto flex items-center gap-2 font-lato">
+              Get Started <i className="fa-solid fa-arrow-right text-[#F8F9FB]"></i>
+            </button>
           </div>
-          {conents.isButton && (
-            <div className="w-[286] flex justify-center items-center gap-[16px] h-[46px]">
-              <button className="border-[1px] border-[#6596A1] rounded-[8px] px-[20px] py-[8px] text-[#6596A1] font-lato">
-                Learn More
-              </button>
-              <button className="bg-[#6596A1] rounded-[8px] py-[8px] px-[20px] text-white border-[1px] w-[148px] h-[46px] flex items-center gap-[6px] font-lato">
-                Get Started{" "}
-                <i className="fa-solid fa-arrow-right text-[#F8F9FB]"></i>
-              </button>
-            </div>
-          )}
-          {conents.isSearchBar && (
-            <div className=" w-1/2">
-              <form className="w-full mx-auto">
-                <div className="flex">
-                  <div className="relative w-full">
-                    <input
-                      type="search"
-                      id="location-search"
-                      className="block p-2.5 w-full z-20 text-sm rounded-lg "
-                      placeholder="Enter your email"
-                      required
-                    />
-                    <button
-                      type="submit"
-                      className=" w-1/3 absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white rounded-e-lg bg-[#6596A1]"
-                    >
-                      Subscribe
-                      <span className="sr-only">Search</span>
-                    </button>
-                  </div>
+        )}
+        {conents.isSearchBar && (
+          <div className="w-full sm:w-1/2">
+            <form className="w-full mx-auto">
+              <div className="flex">
+                <div className="relative w-full">
+                  <input
+                    type="search"
+                    id="location-search"
+                    className="block p-2.5 w-full text-sm rounded-lg"
+                    placeholder="Enter your email"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white rounded-e-lg bg-[#6596A1]"
+                  >
+                    Subscribe
+                    <span className="sr-only">Search</span>
+                  </button>
                 </div>
-              </form>
-            </div>
-          )}
-        </div>
+              </div>
+            </form>
+          </div>
+        )}
       </div>
-      {conents.image && (
-        <div className="">
-          <img src={conents.image} alt="" />
-        </div>
-      )}
-      {conents.card && (
-        <CardOne cardData={cardData}/>
-      )}
     </div>
+    {conents.image && (
+      <div className="w-full max-w-[500px]">
+        <img src={conents.image} alt="" className="w-full h-auto object-cover" />
+      </div>
+    )}
+    {conents.card && <CardOne cardData={conents.cardData} />}
+  </div>
+  
   );
 };
 
--- src/components/cards/CardOne.jsx
+++ src/components/cards/CardOne.jsx
@@ -1,38 +1,44 @@
 import React from "react";
 import image from "../../assets/blogHerophoto.svg";
-import second from '../../assets/aboutCard3img.svg'
+import second from "../../assets/aboutCard3img.svg";
 
 const CardOne = ({ cardData }) => {
-  console.log(cardData)
-  console.log(image)
   return (
-    <div className={`w-[1440px] gap-[64px] flex justify-center items-center ${cardData.isBg && 'bg-[#ccdce0]'} rounded-lg`}>
+    <div
+      className={`w-full max-w-[1440px] gap-8 flex flex-col sm:flex-row justify-center items-center ${
+        cardData.isBg && "bg-[#ccdce0]"
+      } rounded-lg p-4 sm:p-8`}
+    >
       <div
-        className={`w-[1221px] flex ${
-          cardData.isImageLeft && "flex-row-reverse"
-        }  justify-center gap-40`}
+        className={`w-full flex flex-col sm:flex-row ${
+          cardData.isImageLeft ? "sm:flex-row-reverse" : ""
+        } justify-center gap-10`}
       >
-        <div className="flex flex-col justify-center gap-[20px]">
-          <div className="w-[400px]">
-            <h1 className="font-futura text-[31px] text-[#223236] font-bold">
+        <div className="flex flex-col justify-center gap-5">
+          <div className="w-full max-w-[400px]">
+            <h1 className="font-futura text-[24px] sm:text-[31px] text-[#223236] font-bold">
               {cardData.title}
             </h1>
           </div>
-          <div className="w-[434px]">
-            <p className="font-lato text-[16px] text-[#7F7D7D]">
+          <div className="w-full max-w-[434px]">
+            <p className="font-lato text-[14px] sm:text-[16px] text-[#7F7D7D]">
               {cardData.text}
             </p>
           </div>
           {cardData.isBtn && (
             <div>
-              <button className="bg-[#6596A1] rounded-[8px] py-[8px] px-[20px] text-white border-[1px] w-[148px] h-[46px] flex items-center gap-[6px] font-lato">
+              <button className="bg-[#6596A1] rounded-[8px] py-2 px-4 sm:px-[20px] text-white border-[1px] w-full sm:w-[148px] h-[40px] sm:h-[46px] flex items-center justify-center gap-[6px] font-lato">
                 Read more
               </button>
             </div>
           )}
         </div>
         <div className="flex justify-center items-center">
-          <img className="size-96" src={cardData.image} alt="Blog Image" />
+          <img
+            className="w-full h-auto max-w-[300px] sm:max-w-[400px]"
+            src={cardData.image}
+            alt="Blog Image"
+          />
         </div>
       </div>
     </div>
