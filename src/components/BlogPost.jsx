036b72d93aec2b4a5b637c74a1127f7f002c4ab0
Nokihera <minzarnihtut811@gmail.com>
2024-08-18 00:28:21 +0700

    testionial component done

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -4,7 +4,7 @@ import img2 from "../assets/blogImg.svg";
 import img3 from "../assets/blogImg2.svg";
 const BlogPost = () => {
   return (
-    <div className="w-[1440px] h-[991px] gap-[64px] py-[100px] flex flex-col items-center">
+    <div className="w-[1440px] h-[991px] gap-[64px] py-[100px] flex flex-col items-center justify-center content-center justify-items-center">
       <div className="flex flex-col gap[16px] items-center">
         <h1 className="font-futura text-[20px] text-[#6596A1] border-[1px] border-[#C4C4C4] rounded-[20px] w-[192px] h-[38px] py-[5px] px-[15px] flex justify-center items-center ">
           Magic Unleashed
@@ -17,9 +17,11 @@ const BlogPost = () => {
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's{" "}
         </p>
-        <div className="w-[1218px] h-[543px] gap-[32px] grid grid-flow-row grid-cols-3 items-center mt-[64px]">
-          <div className="flex flex-col justify-center shadow-custom-2 rounded-[20px]">
+        <div className="w-[1218px] h-[575px] gap-[32px] grid grid-flow-row grid-cols-3 items-center mt-[64px]">
+          <div className="flex flex-col justify-center shadow-custom-2 rounded-[20px]  ">
+            <div>
             <img src={img1} alt="" />
+            </div>
             <div className="w-[384.67px] h-[310px] py-[24px] px-[32px] gap-[24px] flex flex-col bg-[#F0F5F6] rounded-[20px]">
               <div className="flex gap-[16px] items-center">
                 <span className="w-[79px] h-[28px] rounded-[5px] py-[2px] px-[8px] bg-[#6596A1] text-white font-lato text-[14px] flex justify-center ">
@@ -68,7 +70,7 @@ const BlogPost = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-center shadow-custom-2 rounded-[20px] relative">
+          <div className="flex flex-col justify-center shadow-custom-2 rounded-[20px] ">
             <img
               src={img3}
               alt=""
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,9 +4,12 @@ import smallProfile1 from "../assets/smallProfile1.svg";
 import smallProfile2 from "../assets/smallProfile2.svg";
 import smallProfile3 from "../assets/smallProfile3.svg";
 import smallProfile4 from "../assets/smallProfile4.svg";
+import icon from '../assets/Icon.svg';
 const Testimonial = () => {
+  const currentRating = 4;
+  const numbers = [1, 2, 3, 4, 5];
   return (
-    <div className="w-[1218px] h-[100px] mx-auto grid grid-flow-row grid-cols-3 items-center relative content-center">
+    <div className="w-[1218px] h-[100px] mx-auto grid grid-flow-row grid-cols-3 items-center relative content-center mt-5">
       <div className="flex flex-col justify-center items-start gap-[16px] w-[248px] h-[96px] mx-auto ">
         <div className="relative w-[155px] h-[35px] ">
           <img src={smallProfile} alt="" className="absolute left-[40px]" />
@@ -23,15 +26,14 @@ const Testimonial = () => {
       </div>
       <div className="w-[248px] h-[100px] gap-[24px] flex flex-col mx-auto">
         <div className="flex justify-center">
-          {Array.from({ length: 5 }, (_, i) => (
+          {numbers.map((number) => (
             <svg
-              key={i}
               xmlns="http://www.w3.org/2000/svg"
-              fill={`${Array.from({length:4}, (_, i) => ('yellow'))[i]}`}
+              fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="yellow"
-              className="size-6"
+              stroke="gray-500"
+              className={`size-6 ${number <= currentRating ? "fill-yellow-400" : "fill-gray-500"}`}
             >
               <path
                 strokeLinecap="round"
@@ -41,7 +43,14 @@ const Testimonial = () => {
             </svg>
           ))}
         </div>
+        <div>
+          <p className="text-center text-[18px] font-lato text-[#7F7D7D]">5 out of 5 stars from 123 reviews from our users</p>
+        </div>
       </div>
+      <div className="flex flex-col w-[248px] h-[92px] mx-auto gap-[16px]">
+          <img src={icon} alt="" className="w-[18px] h-[20px] mx-auto"/>
+          <p className="font-lato text-[18px] text-[#7F7D7D] text-center">Repurpose analytics with the possibility to come up.</p>
+        </div>
     </div>
   );
 };
